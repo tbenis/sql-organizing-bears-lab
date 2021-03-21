6aa621ecb34f6836abbda303dc5c009c4eef80bb
Benis Tambe <will.jenifer@yahoo.com>
2021-03-21 06:02:21 -0400

    commit tree edce8adce0ad5e1d77268aaf70dc708cfc735ecd

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color Text,
+    temperament TEXT,
+    alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 16, "M", "Brown", "Collected", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 15, "F", "Brown", "Loud", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 16, "F", "White", "Sensitive", 1);
+INSERT INTO bears VALUES (4, "Sargeant Brown", 8, "M", "Red", "Strict", 0);
+INSERT INTO bears VALUES (5, "Melissa", 1, "F", "White", "Nice", 1);
+INSERT INTO bears VALUES (6, "Grinch", 4, "M", "Red", "Fierce", 1);
+INSERT INTO bears VALUES (7, "Wendy", 7, "F", "Golden", "Calm", 1);
+INSERT INTO bears VALUES (8, null, 28, "F", "Red", "not known", 0);
